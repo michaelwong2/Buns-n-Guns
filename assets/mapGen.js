60b1fcb627bc1e092e96122fa18c6a1ce6d3863f
Michael T. Wong <19mtw1@tcl216-12.cs.williams.edu>
2017-01-16 15:57:43 -0500

    map generation

---
+++ assets/mapGen.js
@@ -0,0 +1,47 @@
+Game.mapGen = {
+  _currSeed: null,
+  setSeed: function(s){
+    this._currSeed = s;
+    ROT.RNG.setSeed(this._currSeed);
+  },
+  getSeed: function(){
+    return this._currSeed;
+  },
+  genNewRandomSeed: function(){
+     this.setSeed(5 + Math.floor(ROT.RNG.getUniform()*100000));
+  },
+  newMap: function(h, w){
+    if(this._currSeed == null){
+        this.genNewRandomSeed();
+    }
+
+    var generator = new ROT.Map.Cellular(h, w);
+    generator.randomize(0.5);
+
+    for(var i = 0; i < 10; i++){
+      generator.create();
+    }
+
+    var tileArray = Game.util.init2DArray(h,w,Game.Tile.nullTile);
+
+    generator.create(function(x,y,val){
+      if(val === 0){
+        tileArray[x][y] = Game.Tile.wallTile;
+      }else{
+        tileArray[x][y] = Game.Tile.floorTile;
+      }
+    });
+
+    return new Game.Map(tileArray);
+  },
+  loadPreviousMap: function(seed){
+    this.setSeed(seed);
+    var oldmap = this.newMap();
+
+    for(var k in Game.DATASTORE.ENTITIES){
+      oldmap.addEntity(Game.DATASTORE.ENTITIES[k]);
+    }
+
+    return oldmap;
+  },
+}
--- assets/message.js
+++ assets/message.js
@@ -1,56 +1,19 @@
 Game.Message = {
   attr:{
-    fresh: [],
-    stale: [],
-    archive: [],
-    archiveLimit: 200
+    messages: []
   },
   render: function(display){
     display.clear();
-
-    var dispRowMax = display._options.height - 1;
-    var dispColMax = display._options.width - 2;
-    var dispRow = 0;
-    var freshMsgIdx = 0;
-    var staleMsgIdx = 0;
-    // fresh messages in white
-    for (freshMsgIdx = 0; freshMsgIdx < this.attr.fresh.length && dispRow < dispRowMax; freshMsgIdx++) {
-      dispRow += display.drawText(1,dispRow,'%c{#fff}%b{#000}'+this.attr.fresh[freshMsgIdx]+'%c{}%b{}',79);
-    }
-    // stale messages in grey
-    for (staleMsgIdx = 0; staleMsgIdx < this.attr.stale.length && dispRow < dispRowMax; staleMsgIdx++) {
-      dispRow += display.drawText(1,dispRow,'%c{#aaa}%b{#000}'+this.attr.fresh[staleMsgIdx]+'%c{}%b{}',79);
-    }
-  },
-  age: function(lastStaleMessageIdx){
-     if (this.attr.stale.length > 0) {
-       this.attr.archive.unshift(this.attr.stale.pop());
-     }
-     // archive any additional stale messages that didn't get shown
-     while (this.attr.stale.length > lastStaleMessageIdx) {
-       this.attr.archive.unshift(this.attr.stale.pop());
-     }
-     // just dump messages that are too old for the archive
-     while (this.attr.stale.length > this.attr.archiveLimit) {
-       this.attr.archive.pop();
-     }
-     // move fresh messages to stale messages
-     while (this.attr.fresh.length > 0) {
-       this.attr.stale.unshift(this.attr.fresh.shift());
-     }
+    display.drawText(1,1, ":)");
   },
   send: function(msg){
     this.set(msg);
     this.render(Game.getDisplay("message"));
   },
   set: function(msg){
-    this.attr.fresh.push(msg);
+    this.attr.messages.shift(msg);
   },
   clear: function(){
-    this.attr.fresh = [];
-    this.attr.stale = [];
-  },
-  getArchive: function(){
-    return this.attr.archive;
+    this.attr.messages = [];
   }
 }
